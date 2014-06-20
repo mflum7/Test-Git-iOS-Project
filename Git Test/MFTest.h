9a6a0e7a91d88b78b8bb209e5663cc2ee53ad15e
Michael Flum <michaelflum@lizzys-air.home>
2014-06-19 22:50:32 -0700

    added a test class

---
+++ Git Test/MFTest.h
@@ -0,0 +1,13 @@
+//
+//  MFTest.h
+//  Git Test
+//
+//  Created by Michael Flum on 6/19/14.
+//  Copyright (c) 2014 Michael Flum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFTest : NSObject
+
+@end
---
+++ Git Test/MFTest.m
@@ -0,0 +1,13 @@
+//
+//  MFTest.m
+//  Git Test
+//
+//  Created by Michael Flum on 6/19/14.
+//  Copyright (c) 2014 Michael Flum. All rights reserved.
+//
+
+#import "MFTest.h"
+
+@implementation MFTest
+
+@end
